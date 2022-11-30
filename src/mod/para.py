8874bd65d1028d373918fadcd58c3ba26ebc2617
acguy <taniguchi.shohei.412@s.kyushu-u.ac.jp>
2022-11-30 15:15:11 +0900

    update

--- src/mod/para.py
+++ src/mod/para.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 # write down some parameter
 
 # number of girds (x, y) [-]
@@ -12,6 +13,9 @@ Lz = 1
 # time step [sec]
 dt = 0.1
 
+# end of ime [sec]
+time = 10
+
 # viscousity coefficient [m^2/s]
 nu = 1e-6
 
@@ -20,7 +24,8 @@ x = np.linspace(0,Lx,nx+1)
 z = np.linspace(0,Lz,nz+1)
 xx, zz = np.meshgrid(x,z)
 
+
 # scalar & fulux
-t = np.zeros((nx  ,nz  ,3)) # [K]
-u = np.zeros((nx+1,nz  ,3)) # [m/s]
-w = np.zeros((nx  ,nz+1,3)) # [m/s]
+t = np.zeros((nx  ,nz  )) # [K]
+u = np.zeros((nx+1,nz  )) # [m/s]
+w = np.zeros((nx  ,nz+1)) # [m/s]
