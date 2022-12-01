65c8fc8cce4af63d84e0fa7a169b7a0fa1009356
inspiron <heisyou123@gmail.com>
2022-12-01 16:14:52 +0900

    update

--- src/main.py
+++ src/main.py
@@ -5,6 +5,22 @@ import matplotlib.pyplot as plt
 from mod import *
 
 if __name__=="__main__":
+    # I.C.
+    print(t)
+    t[:]=1
+    print(t)
+
+    
     time_num = int(time/dt)
     for t in range(1,time_num+1):
-        print(dt*t)
+        for k in range(nz):
+            for i in range(nx):
+                
+                continue
+
+
+    #plt.scatter(xx,zz,color="y")
+    #plt.scatter(xxu,zzu,color="b",marker=f"$u$")
+    #plt.scatter(xxw,zzw,color="g",marker=f"$w$")
+    #plt.scatter(xxs,zzs,color="r",marker=f"$T$")
+    #plt.show()
--- src/mod/para.py
+++ src/mod/para.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 # write down some parameter
 
 # number of girds (x, y) [-]
-nx=4
-nz=4
+nz=3
+nx=3
 
 # Length (x ,y) [m]
 Lx = 1
@@ -19,13 +19,40 @@ time = 1
 # viscousity coefficient [m^2/s]
 nu = 1e-6
 
-# set up
-x = np.linspace(0,Lx,nx+1)
-z = np.linspace(0,Lz,nz+1)
-xx, zz = np.meshgrid(x,z)
-
+# diffusivity coefficient [m^2/s]
+Kh = 1e-6
+Kv = 1e-6
 
 # scalar & fulux
 t = np.zeros((nx  ,nz  )) # [K]
 u = np.zeros((nx+1,nz  )) # [m/s]
 w = np.zeros((nx  ,nz+1)) # [m/s]
+
+tp = np.zeros((nx  ,nz  )) # [K]
+up = np.zeros((nx+1,nz  )) # [m/s]
+wp = np.zeros((nx  ,nz+1)) # [m/s]
+
+tm = np.zeros((nx  ,nz  )) # [K]
+um = np.zeros((nx+1,nz  )) # [m/s]
+wm = np.zeros((nx  ,nz+1)) # [m/s]
+
+# set up grid
+# along with grid line
+x = np.linspace(0,Lx,nx+1)
+z = np.linspace(0,Lz,nz+1)
+xx, zz = np.meshgrid(x,z)
+
+# vector of U
+xu = np.array([ x[i] for i in range(nx+1) ])
+zu = np.array([ 0.5*(z[k+1]+z[k]) for k in range(nz) ])
+xxu, zzu = np.meshgrid(xu,zu)
+
+# vector of W
+xw = np.array([ 0.5*(x[i]+x[i+1]) for i in range(nx) ])
+zw = np.array([ z[k] for k in range(nz+1) ])
+xxw, zzw = np.meshgrid(xw,zw)
+
+# scallar 
+xs = np.array([ 0.5*(xu[i]+xu[i+1]) for i in range(nx) ])
+zs = np.array([ 0.5*(zw[k+1]+zw[k]) for k in range(nz) ])
+xxs, zzs = np.meshgrid(xs,zs)
